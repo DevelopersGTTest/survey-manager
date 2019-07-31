947ee08bbdf4652aaa9b027abd4f787d664775b6
Pablo Jacobo <pablojacobo1998@gmail.com>
2019-07-31 15:03:16 -0600

    basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './core/components/auth/auth.component';
+import { ResultsComponent } from './core/components/results/results.component';
+import { SurveyComponent } from './core/components/survey/survey.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    ResultsComponent,
+    SurveyComponent
   ],
   imports: [
     BrowserModule
