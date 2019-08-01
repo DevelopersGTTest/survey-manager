2cb7671c16d96cecec5310b8d490ed1e65fd9155
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-08-01 00:21:24 -0600

    fixed template

--- src/app/core/components/survey/survey.component.ts
+++ src/app/core/components/survey/survey.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SurveyComponent implements OnInit {
 
   private showInputOne : boolean = false
+  private showInputTwo : boolean = false
 
   constructor() { }
 
@@ -15,11 +16,20 @@ export class SurveyComponent implements OnInit {
   }
 
   noTraditionalism(evt) {
-    console.log('el evento es', evt.target.name );
-    ( evt.target.checked === true && evt.target.id  == "notAgreed" ) 
+    console.log('this event is', evt.target.name );
+    ( evt.target.checked === true 
+      && evt.target.id  == "notAgreed" ) 
       ? this.showInputOne = true 
       : this.showInputOne = false ;
 
+  }
+
+  noDemocracyAply(evt){
+    console.log('this event is', evt.target.id );
+    ( evt.target.checked === true 
+      && evt.target.id  == "notDemocracy" ) 
+      ? this.showInputTwo = true 
+      : this.showInputTwo = false ;
   }
 
 }
