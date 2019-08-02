0f7deb5782cd1649024aa504773ac25a09ad5ccc
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-08-02 00:41:56 -0600

    static table

--- src/app/core/components/results/results.component.ts
+++ src/app/core/components/results/results.component.ts
@@ -13,6 +13,18 @@ export class ResultsComponent implements OnInit {
 
   public radarChartLabels: any[] = ['Tradicionalista por la familia', 'Monopolios', 'Marcas', 'Opinion democratica'];
 
+  private dataArray: any[] =[
+    { id : 1 , family: "si", age: 20 , mark : "Coca-Cola", democracy: "no" },
+    { id : 2,  family: "si", age: 19 , mark : "Coca-Cola", democracy: "si" },
+    { id : 3,  family: "si", age: 19 , mark : "Coca-Cola", democracy: "no" },
+    { id : 4,  family: "no", age: 19 , mark : "Coca-Cola", democracy: "si" },
+    { id : 5, family: "si", age: 28 , mark : "Coca-Cola", democracy: "si" },
+    { id : 6, family: "si", age: 50 , mark : "Coca-Cola", democracy: "si" },
+    { id : 7, family: "no", age: 18 , mark : "Coca-Cola", democracy: "si" },
+    { id : 8,  family: "si", age: 20 , mark : "Coca-Cola", democracy: "si" },
+
+  ] 
+
   public radarChartData: any[] = [
     { data: [59, 90, 81, 56 ], label: 'izquierda' },
     { data: [48, 40, 19, 96 ], label: 'centro' },
